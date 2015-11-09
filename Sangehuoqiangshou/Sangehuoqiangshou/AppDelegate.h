99dd7ce6a36401f45bb1e9f4be96316d47721811
lanou3g <18833886059@163.com>
2015-11-09 09:52:28 +0800

    11月9日

---
+++ Sangehuoqiangshou/Sangehuoqiangshou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sangehuoqiangshou
+//
+//  Created by lanou3g on 15/11/9.
+//  Copyright © 2015年 钮海雷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
